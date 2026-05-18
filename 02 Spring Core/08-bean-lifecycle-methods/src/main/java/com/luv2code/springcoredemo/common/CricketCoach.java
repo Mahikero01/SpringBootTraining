26bfd366bf7c4e2fa4ea1c06bd09f6467f3015e0
Mahikero01 <softwaredevelopment03@gmail.com>
2026-05-19 06:30:27 +0800

    Retro Topics

---
+++ 02 Spring Core/08-bean-lifecycle-methods/src/main/java/com/luv2code/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,30 @@
+package com.luv2code.springcoredemo.common;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.println("CricketCoach constructor");
+    }
+
+    @PostConstruct
+    public void doMyStartupStuff() {
+        System.out.println("In doMyStartupStuff(): " + getClass().getSimpleName());
+    }
+
+    @PreDestroy
+    public void doMyCleanUpStuff() {
+        System.out.println("In doMyCleanUpStuff(): " + getClass().getSimpleName());
+    }
+
+    
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes";
+    }
+}
---
+++ 02 Spring Core/09-java-config-bean/src/main/java/com/luv2code/springcoredemo/common/TrackCoach.java
@@ -0,0 +1,17 @@
+package com.luv2code.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TrackCoach implements Coach {
+
+    public TrackCoach() {
+        System.out.println("TrackCoach constructor");
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Run a hard 5k";
+    }
+}
